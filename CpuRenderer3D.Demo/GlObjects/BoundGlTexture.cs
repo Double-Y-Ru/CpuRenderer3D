ef940e93c531cead7be05884013d6d0a0e72f180
Andrey Lemin <andrey.lemin@softsmile.com>
2024-11-18 12:23:47 +0300

    Get rid of Bytemap

--- CpuRenderer3D.Demo/GlObjects/BoundGlTexture.cs
+++ CpuRenderer3D.Demo/GlObjects/BoundGlTexture.cs
@@ -33,21 +33,21 @@ namespace CpuRenderer3D.Demo.GlObjects
             GL.GenerateMipmap(GenerateMipmapTarget.Texture2D);
         }
 
-        public void SetImage(Bytemap bytemap)
+        public void SetImage(Buffer<int> buffer)
         {
-            GL.TexImage2D(TextureTarget.Texture2D, 0, PixelInternalFormat.Rgba, bytemap.Width, bytemap.Height, 0, PixelFormat.Rgba, PixelType.UnsignedByte, bytemap.GetData());
-            GL.GenerateMipmap(GenerateMipmapTarget.Texture2D);
-        }
-
-        public void UpdateImage(Bytemap bytemap)
-        {
-            GL.TexSubImage2D(TextureTarget.Texture2D, 0, 0, 0, bytemap.Width, bytemap.Height, PixelFormat.Rgba, PixelType.UnsignedByte, bytemap.GetData());
+            GL.TexImage2D(TextureTarget.Texture2D, 0, PixelInternalFormat.Rgba, buffer.Width, buffer.Height, 0, PixelFormat.RgbaInteger, PixelType.UnsignedInt, buffer.GetData());
             GL.GenerateMipmap(GenerateMipmapTarget.Texture2D);
         }
 
         public void UpdateImage(Buffer<Vector4> buffer)
         {
             GL.TexSubImage2D(TextureTarget.Texture2D, 0, 0, 0, buffer.Width, buffer.Height, PixelFormat.Rgba, PixelType.Float, buffer.GetData());
+            GL.GenerateMipmap(GenerateMipmapTarget.Texture2D);
+        }
+
+        public void UpdateImage(Buffer<int> buffer)
+        {
+            GL.TexSubImage2D(TextureTarget.Texture2D, 0, 0, 0, buffer.Width, buffer.Height, PixelFormat.RgbaInteger, PixelType.UnsignedInt, buffer.GetData());
             GL.GenerateMipmap(GenerateMipmapTarget.Texture2D);
         }
     }
