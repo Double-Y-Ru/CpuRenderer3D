9ab8684118701a9973450edb95e90ce646f64414
Andrey Lemin <anlemin@mail.ru>
2024-12-18 23:15:56 +0300

    Add different cam matrices

--- DoubleY.CpuRenderer3D/Camera.cs
+++ DoubleY.CpuRenderer3D/Camera.cs
@@ -25,20 +25,59 @@ namespace DoubleY.CpuRenderer3D
 
         public static Camera CreatePerspective(Transform transform, float aspect, float fieldOfViewRadians, float nearPlane, float farPlane)
         {
-            // return new Camera(transform, Matrix4x4.CreatePerspectiveFieldOfView(fieldOfViewRadians, aspect, nearPlane, farPlane)); // The difference is M44 == 0f
+            Matrix4x4 matrixOld = CreatePerspectiveFieldOfViewStandaard(fieldOfViewRadians, aspect, nearPlane, farPlane);
+            Matrix4x4 matrixNew = CreatePerspectiveFieldOfViewLegacy(fieldOfViewRadians, aspect, nearPlane, farPlane);
 
-            float yScale = 1.0f / MathF.Tan(fieldOfViewRadians / 2.0f) * aspect;
+            return new Camera(transform, Matrix4x4.CreatePerspectiveFieldOfView(fieldOfViewRadians, aspect, nearPlane, farPlane));
+        }
+
+        public static Camera CreatePerspectiveLegacy(Transform transform, float aspect, float fieldOfViewRadians, float nearPlane, float farPlane)
+        {
+            return new Camera(transform, CreatePerspectiveFieldOfViewLegacy(fieldOfViewRadians, aspect, nearPlane, farPlane));
+        }
+
+        public static Matrix4x4 CreatePerspectiveFieldOfViewStandaard(float fieldOfView, float aspectRatio, float nearPlaneDistance, float farPlaneDistance)
+        {
+            if (fieldOfView <= 0f || fieldOfView >= (float)Math.PI)
+            {
+                throw new ArgumentOutOfRangeException("fieldOfView");
+            }
+            if (nearPlaneDistance <= 0f)
+            {
+                throw new ArgumentOutOfRangeException("nearPlaneDistance");
+            }
+            if (farPlaneDistance <= 0f)
+            {
+                throw new ArgumentOutOfRangeException("farPlaneDistance");
+            }
+            if (nearPlaneDistance >= farPlaneDistance)
+            {
+                throw new ArgumentOutOfRangeException("nearPlaneDistance");
+            }
+            float yScale = 1f / MathF.Tan(fieldOfView * 0.5f);
+            float xScale = yScale / aspectRatio;
+            float zScale = farPlaneDistance / (nearPlaneDistance - farPlaneDistance);
+
+            return new Matrix4x4(
+                xScale, 0f, 0f, 0f,
+                0f, yScale, 0f, 0f,
+                0f, 0f, zScale, -1f,
+                0f, 0f, nearPlaneDistance * zScale, 0f
+            );
+        }
+
+        public static Matrix4x4 CreatePerspectiveFieldOfViewLegacy(float fieldOfViewRadians, float aspect, float nearPlane, float farPlane)
+        {
+            float yScale = 1.0f / MathF.Tan(fieldOfViewRadians / 2.0f);
             float xScale = yScale / aspect;
             float frustumLength = farPlane - nearPlane;
 
-            Matrix4x4 m = new Matrix4x4(
-                xScale, 0, 0, 0,
-                0, yScale, 0, 0,
-                0, 0, -((farPlane + nearPlane) / frustumLength), -1.0f,
-                0, 0, -(2.0f * nearPlane * farPlane / frustumLength), 1f
+            return new Matrix4x4(
+                xScale, 0f, 0f, 0f,
+                0f, yScale, 0f, 0f,
+                0f, 0f, -((farPlane + nearPlane) / frustumLength), -1f,
+                0f, 0f, -(2f * nearPlane * farPlane / frustumLength), 1f
             );
-
-            return new Camera(transform, m);
         }
 
         public static Camera CreateOrthographic(Transform transform, float width, float height, float nearPlane, float farPlane)
